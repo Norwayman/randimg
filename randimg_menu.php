552ec29b6f3526e9127a2305064c3b2476765f56
septor <patrickweaver@gmail.com>
2012-05-21 18:18:44 -0400

    initial commit

---
+++ randimg_menu.php
@@ -0,0 +1,21 @@
+﻿<?php
+if (!defined('e107_INIT')) { exit; }
+
+define("RIMID", e_PLUGIN."randimg_menu/images/");
+
+$height = "150px";
+$width = "150px";
+
+$images = array();
+foreach(glob("{".RIMID."*.jpg,".RIMID."*.gif,".RIMID."*.png}", GLOB_BRACE) as $image_file){
+	array_push($images, str_replace(RIMID, "", $image_file));
+}
+
+if(!empty($images[array_rand($images)])){
+	$text = "<a href='".RIMID.$images[array_rand($images)]."'><img src='".RIMID.$images[array_rand($images)]."' style='width:".$width."; height:".$height."; border:0;'></a>";
+}else{
+	$text = "No images found. Add some to the images directory!";
+}
+
+$ns->tablerender("Random Image", "<div style='text-align:center;'>".$text."</div>", 'randimg_menu');
+?>
